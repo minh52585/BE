d4469f952e8dd5af4c9f94257541f91a8c410236
unknown <{minhpnph52585@gmail.com>
2025-05-19 20:50:11 +0700

    update

--- src/routes/product.router.js
+++ src/routes/product.router.js
@@ -4,8 +4,8 @@ const productController = require('../controllers/productController');
 
 router.get('/', productController.getAllProducts);
 router.get('/:id', productController.getProductById);
-router.post('/', productController.createProduct);
-router.put('/:id', productController.updateProduct);
+router.post('/add', productController.createProduct);
+router.put('/edit/:id', productController.updateProduct);
 router.delete('/:id', productController.deleteProduct);
 
 module.exports = router;
